b06d7f0748b5ba9820b71aeda2cad004d27e5c1d
justen <justen@justens-MacBook-Pro.local>
2019-08-31 20:39:47 -1000

    added favicon

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  siteTitle: 'Justen Nakamoto | Portfolio', // <title>
+  siteTitle: 'Justen Nakamoto | Full Stack Web Developer', // <title>
   manifestName: 'Justen Nakamoto',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  manifestIcon: 'src/assets/img/fav.png',
+  // pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Justen Nakamoto',
   subHeading: 'Full Stack Web Developer',
   // social
